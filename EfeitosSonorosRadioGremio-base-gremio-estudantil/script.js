5aedfd7d73ad87f6369ac8c711f18ed073801921
JoaoEmerson0 <113241260+JoaoEmerson0@users.noreply.github.com>
2023-11-09 10:51:56 -0300

    Add files via upload

--- EfeitosSonorosRadioGremio-base-gremio-estudantil/script.js
+++ EfeitosSonorosRadioGremio-base-gremio-estudantil/script.js
@@ -1,4 +1,4 @@
-document.getElementById("btn-aplauso").addEventListener("click", function(){
-    var aplauso = document.getElementById("som_tecla_aplausos")
-    aplauso.onplay();
-})
+function tocaSom(id){
+    var som = document.getElementById(id)
+    som.play();
+}
